084ae623101a216b43f2d694fe4cc9cec761f2a6
Phillip <phillip.goellner@protonmail.com>
2024-12-02 20:41:48 +0100

    Solve day 2

--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,5 @@
 const run = async () => {
-    for (let i = 1; i <= 1; i++) {
+    for (let i = 1; i <= 2; i++) {
         const puzzleInput = (await Bun.file(`inputs/day${i}.txt`).text()).split("\n");
 
         const resultPart1 = (await import(`./day${i}.ts`)).part1(puzzleInput);
