196cdf3002cec5e3bdb58bf95af1728b0612d109
Phillip <phillip.goellner@protonmail.com>
2024-12-09 09:29:19 +0100

    Solve day 8 myself by debugging with the stolen code fragment (from https://topaz.github.io/paste/#XQAAAQAbBwAAAAAAAAAxm8oZxjYXx5KZAhVK8ES441FufcheOCDCxYbNiDGR6FHPigEM5QyR1wLS0+zyTM6V3/JoxsAR+MzmCG/3lKRjYbMxWZB/qmIjDbE5iBrR27tvfXllg1Ao7+lZ3ItPCGyECsSW8XqD1CSEtyoJc+rohDh2kR4i4FE82HKSHK3QYdPY42tLxcwwJqKIHayTvzTV+zQcblq7+znoN0DBGrnC1qdxoMnd5M9zoziTtIiH1uNIJYosuismNz3Lx/iyp+gvQyQxrqef/LnJ9v+w++9MQFbSC80WEsTXczIyemmEszuVniiSQT6aUs8Jqat7A9y8ydXb8OL667qCGUON6TaohD89PHq4Y6EdhJ82+VBlcJ0wbFuCLt3WJri+ZPIAP+6EA1ogjrYUK+pNn2GJRvP1yE55sk4XiTtwh+Tj1JlYlqSnq6XdCzSX+spGZWoYOQiMMCWxIYvF6nhcxWmyLa7G/nAkxK/fbP9vyzIdNLaSDG6sLojK6BG8amuXdez618rkJQ38KPseBjUN5ThX/zYCFXtNZfLIYvWGEn+8n4iGpl9pP2uhVEbZUilCvaNhF9qF1+1OgoufsWlg1PP3FOt30hFx/HOE7QQY2GxnieMK9mlJmwb81ARZ6L204IAqqum3OdZ3xRQ/r/9AkpONYGXhstezQv5Y+aGRnszNqYCbRsp4+RAocUg8ZLu96e/+sONRi7eWWkadP7GF/+kLzbE=)

--- src/day8.ts
+++ src/day8.ts
@@ -82,7 +82,7 @@ const antinodeLocationsWithResonance = (antennaLocations: Location[] | undefined
     const cartesianLocations: Location[][] = cartesian([antennaLocations, antennaLocations]);
 
     const isValidLocation = (newLocation: Location) => (newLocation.row >= 0 && newLocation.col >= 0) &&
-        (newLocation.row <= maxRow && newLocation.col <= maxCol);
+        (newLocation.row < maxRow && newLocation.col < maxCol);
 
     for (const [left, right] of cartesianLocations) {
         const rowDelta = left.row - right.row;
@@ -132,42 +132,10 @@ export function part2(lines: string[]): number {
         Array.from(antennas(lines).entries())
             .map(([_, locations]) => antinodeLocationsWithResonance(locations, lines.length, lines[0].length))
             .reduce((l, r) => [...l, ...r], [])
-            .map(location => JSON.stringify(location))
+            .map(location => `${location.row},${location.col}`)
     );
 
-    return part2_stolen(lines.join("\n"));
-}
+    antennas(lines).forEach((locations) => locations.forEach(location => set.add(`${location.row},${location.col}`)))
 
-// Below part has been shamelessly stolen from
-// https://topaz.github.io/paste/#XQAAAQAbBwAAAAAAAAAxm8oZxjYXx5KZAhVK8ES441FufcheOCDCxYbNiDGR6FHPigEM5QyR1wLS0+zyTM6V3/JoxsAR+MzmCG/3lKRjYbMxWZB/qmIjDbE5iBrR27tvfXllg1Ao7+lZ3ItPCGyECsSW8XqD1CSEtyoJc+rohDh2kR4i4FE82HKSHK3QYdPY42tLxcwwJqKIHayTvzTV+zQcblq7+znoN0DBGrnC1qdxoMnd5M9zoziTtIiH1uNIJYosuismNz3Lx/iyp+gvQyQxrqef/LnJ9v+w++9MQFbSC80WEsTXczIyemmEszuVniiSQT6aUs8Jqat7A9y8ydXb8OL667qCGUON6TaohD89PHq4Y6EdhJ82+VBlcJ0wbFuCLt3WJri+ZPIAP+6EA1ogjrYUK+pNn2GJRvP1yE55sk4XiTtwh+Tj1JlYlqSnq6XdCzSX+spGZWoYOQiMMCWxIYvF6nhcxWmyLa7G/nAkxK/fbP9vyzIdNLaSDG6sLojK6BG8amuXdez618rkJQ38KPseBjUN5ThX/zYCFXtNZfLIYvWGEn+8n4iGpl9pP2uhVEbZUilCvaNhF9qF1+1OgoufsWlg1PP3FOt30hFx/HOE7QQY2GxnieMK9mlJmwb81ARZ6L204IAqqum3OdZ3xRQ/r/9AkpONYGXhstezQv5Y+aGRnszNqYCbRsp4+RAocUg8ZLu96e/+sONRi7eWWkadP7GF/+kLzbE=
-// I still have to figure out why I'm missing 5 locations
-
-const parseInput = (rawInput: string) => rawInput.split('\n').map(line => line.split(''))
-const part2_stolen = (rawInput: string) => {
-    const rows = parseInput(rawInput)
-    const towers = new Map<string, string[]>() // 'a' => ['4,5', '7,7'] etc
-    const antinodes = new Set<string>()
-    rows.forEach((row, r) => {
-        row.forEach((char, c) => {
-            if (char === '.') return
-            const towerLocations = towers.get(char) ?? []
-            towerLocations.forEach(locationKey => {
-                const [r2,c2] = locationKey.split(',').map(Number)
-                const slopeTop = c2-c
-                const slopeBottom = r2-r
-                for (let R = 0; R < rows.length; R++) {
-                    const dr = R - r
-                    const dc = dr * slopeTop / slopeBottom
-                    const C = c + dc
-                    if (C < 0 || C >= row.length || C % 1 !== 0) {
-                        continue
-                    }
-                    antinodes.add(`${R},${C}`)
-                }
-            })
-            towerLocations.push(`${r},${c}`)
-            towers.set(char, towerLocations)
-        })
-    })
-    return antinodes.size
+    return set.size;
 }
