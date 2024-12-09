4ff91ad8078a209f20532833d518a13c81a2a1b5
Phillip <phillip.goellner@protonmail.com>
2024-12-09 09:44:21 +0100

    Implement a way to skip slow days

--- src/main.ts
+++ src/main.ts
@@ -1,13 +1,18 @@
+const slowDays = new Set([6, 7]);
+
 const run = async () => {
     for (let i = 1; i <= 8; i++) {
         const puzzleInput = (await Bun.file(`inputs/day${i}.txt`).text()).split("\n");
+        if (!slowDays.has(i)) {
+            const resultPart1 = (await import(`./day${i}.ts`)).part1(puzzleInput);
+            const resultPart2 = (await import(`./day${i}.ts`)).part2(puzzleInput);
 
-        const resultPart1 = (await import(`./day${i}.ts`)).part1(puzzleInput);
-        const resultPart2 = (await import(`./day${i}.ts`)).part2(puzzleInput);
-
-        console.log(`----- Day ${i} -----
+            console.log(`----- Day ${i} -----
 Part 1: ${resultPart1}
-Part 2: ${resultPart2}`)
+Part 2: ${resultPart2}`);
+        } else {
+            console.log(`----- Day ${i} -----\nSkipping...`);
+        }
     }
 }
 
